e381443f304db35d6ebd41683d6637b52849384e
Michal Niewrzal <michal.n@zend.com>
2017-09-04 12:43:14 +0200

    Enable script and style validation for HTML server

--- org.eclipse.bluesky/src/org/eclipse/bluesky/html/HTMLLanguageServer.java
+++ org.eclipse.bluesky/src/org/eclipse/bluesky/html/HTMLLanguageServer.java
@@ -24,6 +24,11 @@ import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.lsp4e.server.ProcessStreamConnectionProvider;
+import org.eclipse.lsp4j.DidChangeConfigurationParams;
+import org.eclipse.lsp4j.InitializeResult;
+import org.eclipse.lsp4j.jsonrpc.messages.Message;
+import org.eclipse.lsp4j.jsonrpc.messages.ResponseMessage;
+import org.eclipse.lsp4j.services.LanguageServer;
 
 public class HTMLLanguageServer extends ProcessStreamConnectionProvider {
 
@@ -31,13 +36,15 @@ public class HTMLLanguageServer extends ProcessStreamConnectionProvider {
 		List<String> commands = new ArrayList<>();
 		commands.add(InitializeLaunchConfigurations.getNodeJsLocation());
 		try {
-			URL url = FileLocator.toFileURL(getClass().getResource("/language-servers/node_modules/vscode-html-languageserver/out/htmlServerMain.js"));
+			URL url = FileLocator.toFileURL(getClass()
+					.getResource("/language-servers/node_modules/vscode-html-languageserver/out/htmlServerMain.js"));
 			commands.add(new java.io.File(url.getPath()).getAbsolutePath());
 			commands.add("--stdio");
 			setCommands(commands);
 			setWorkingDirectory(System.getProperty("user.dir"));
 		} catch (IOException e) {
-			Activator.getDefault().getLog().log(new Status(IStatus.ERROR, Activator.getDefault().getBundle().getSymbolicName(), e.getMessage(), e));
+			Activator.getDefault().getLog().log(
+					new Status(IStatus.ERROR, Activator.getDefault().getBundle().getSymbolicName(), e.getMessage(), e));
 		}
 	}
 
@@ -46,13 +53,37 @@ public class HTMLLanguageServer extends ProcessStreamConnectionProvider {
 		Map<String, Object> map = new HashMap<>();
 		map.put("css", true);
 		map.put("javascript", true);
-		
+
 		Map<String, Object> options = new HashMap<>();
 		options.put("embeddedLanguages", map);
 		options.put("format.enable", true);
 		return options;
 	}
-	
+
+	@Override
+	public void handleMessage(Message message, LanguageServer languageServer, URI rootUri) {
+		if (message instanceof ResponseMessage) {
+			ResponseMessage responseMessage = (ResponseMessage) message;
+			if (responseMessage.getResult() instanceof InitializeResult) {
+				Map<String, Object> options = new HashMap<>();
+				options.put("scripts", true);
+				options.put("styles", true);
+
+				Map<String, Object> validate = new HashMap<>();
+				validate.put("validate", options);
+
+				Map<String, Object> html = new HashMap<>();
+				html.put("html", validate);
+
+				Map<String, Object> settings = new HashMap<>();
+				settings.put("settings", html);
+
+				DidChangeConfigurationParams params = new DidChangeConfigurationParams(settings);
+				languageServer.getWorkspaceService().didChangeConfiguration(params);
+			}
+		}
+	}
+
 	@Override
 	public String toString() {
 		return "HTML Language Server: " + super.toString();
