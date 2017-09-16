7bcd7eb2228d55b826aedd370265cd1357cf74a0
angelozerr <angelo.zerr@gmail.com>
2017-09-16 13:09:55 +0200

    Add tm4e snippet for CSS, JSON, JS, TS, YAML, HTML

---
+++ org.eclipse.bluesky/snippets/typescript/sample.ts
@@ -0,0 +1,9 @@
+import { StringValidator } from "./Validation";
+
+const numberRegexp = /^[0-9]+$/;
+
+export class ZipCodeValidator implements StringValidator {
+    isAcceptable(s: string) {
+        return s.length === 5 && numberRegexp.test(s);
+    }
+}
