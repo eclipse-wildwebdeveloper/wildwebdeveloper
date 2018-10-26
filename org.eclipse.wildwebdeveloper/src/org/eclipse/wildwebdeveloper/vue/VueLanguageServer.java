f77eaa12b73e25b554b7ab59647776623f808436
Gautier de Saint Martin Lacaze <gautier.desaintmartinlacaze@gmail.com>
2018-10-26 17:21:50 +0200

    enable vue support
    Signed-off-by: Gautier de Saint Martin Lacaze <gautier.desaintmartinlacaze@gmail.com>

---
+++ org.eclipse.wildwebdeveloper/src/org/eclipse/wildwebdeveloper/vue/VueLanguageServer.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Eclipse Foundation and others.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Gautier de Saint Martin Lacaze - initial implementation
+ *******************************************************************************/
+package org.eclipse.wildwebdeveloper.vue;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.lsp4e.server.ProcessStreamConnectionProvider;
+import org.eclipse.wildwebdeveloper.Activator;
+import org.eclipse.wildwebdeveloper.InitializeLaunchConfigurations;
+
+public class VueLanguageServer extends ProcessStreamConnectionProvider {
+
+	public VueLanguageServer() {
+		List<String> commands = new ArrayList<>();
+		commands.add(InitializeLaunchConfigurations.getNodeJsLocation());
+		try {
+			URL url = FileLocator.toFileURL(getClass()
+					.getResource("/language-servers/node_modules/vue-language-server/dist/vueServerMain.js"));
+			commands.add(new java.io.File(url.getPath()).getAbsolutePath());
+			commands.add("--stdio");
+			setCommands(commands);
+			setWorkingDirectory(System.getProperty("user.dir"));
+		} catch (IOException e) {
+			Activator.getDefault().getLog().log(
+					new Status(IStatus.ERROR, Activator.getDefault().getBundle().getSymbolicName(), e.getMessage(), e));
+		}
+	}
+
+
+	@Override
+	public String toString() {
+		return "Vue Language Server: " + super.toString();
+	}
+}
