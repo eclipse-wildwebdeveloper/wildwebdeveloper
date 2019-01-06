0195fc698c06a65b0da00fbcc77fde4614334e77
Jonah Graham <jonah@kichwacoders.com>
2019-01-06 12:49:23 +0000

    Issue #111 - Add Debug Adapter tab to launch configurations
    This includes taking advantage of the refactored DSPLaunchDelegate API
    to make it a little cleaner to setup and launch a debug adapter.
    
    Signed-off-by: Jonah Graham <jonah@kichwacoders.com>

--- org.eclipse.wildwebdeveloper/src/org/eclipse/wildwebdeveloper/debug/NodeAttachDebugTabGroup.java
+++ org.eclipse.wildwebdeveloper/src/org/eclipse/wildwebdeveloper/debug/NodeAttachDebugTabGroup.java
@@ -16,6 +16,7 @@ import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup;
 import org.eclipse.debug.ui.CommonTab;
 import org.eclipse.debug.ui.ILaunchConfigurationDialog;
 import org.eclipse.debug.ui.ILaunchConfigurationTab;
+import org.eclipse.lsp4e.debug.launcher.DSPOverrideSettingsTab;
 
 public class NodeAttachDebugTabGroup extends AbstractLaunchConfigurationTabGroup {
 
@@ -23,6 +24,7 @@ public class NodeAttachDebugTabGroup extends AbstractLaunchConfigurationTabGroup
 	public void createTabs(ILaunchConfigurationDialog dialog, String mode) {
 		setTabs(new ILaunchConfigurationTab[] {
 			new AttachTab(),
+			new DSPOverrideSettingsTab(),
 			new CommonTab()
 		});
 	}
--- org.eclipse.wildwebdeveloper/src/org/eclipse/wildwebdeveloper/debug/NodeRunDAPDebugDelegate.java
+++ org.eclipse.wildwebdeveloper/src/org/eclipse/wildwebdeveloper/debug/NodeRunDAPDebugDelegate.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018 Red Hat Inc. and others.
+ * Copyright (c) 2018, 2019 Red Hat Inc. and others.
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -13,8 +13,11 @@
 package org.eclipse.wildwebdeveloper.debug;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.eclipse.core.runtime.CoreException;
@@ -25,17 +28,13 @@ import org.eclipse.core.runtime.Status;
 import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
-import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
 import org.eclipse.debug.core.ILaunchManager;
 import org.eclipse.jface.dialogs.ErrorDialog;
-import org.eclipse.lsp4e.debug.DSPPlugin;
 import org.eclipse.lsp4e.debug.launcher.DSPLaunchDelegate;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.wildwebdeveloper.Activator;
 import org.eclipse.wildwebdeveloper.InitializeLaunchConfigurations;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
 public class NodeRunDAPDebugDelegate extends DSPLaunchDelegate {
@@ -49,49 +48,48 @@ public class NodeRunDAPDebugDelegate extends DSPLaunchDelegate {
 	private static final String ENV = "env"; //$NON-NLS-1$
 
 	@Override
-	public void launch(ILaunchConfiguration configuration, String mode, ILaunch launch, IProgressMonitor monitor) throws CoreException {
-		ILaunchConfigurationWorkingCopy wc = configuration.getWorkingCopy();
+	public void launch(ILaunchConfiguration configuration, String mode, ILaunch launch, IProgressMonitor monitor)
+			throws CoreException {
 		// user settings
-		JsonObject param = new JsonObject();
-		param.addProperty(PROGRAM, wc.getAttribute(PROGRAM, "no program path defined")); //$NON-NLS-1$
-		String argsString = wc.getAttribute(ARGUMENTS, "").trim(); //$NON-NLS-1$
+		Map<String, Object> param = new HashMap<>();
+		param.put(PROGRAM, configuration.getAttribute(PROGRAM, "no program path defined")); //$NON-NLS-1$
+		String argsString = configuration.getAttribute(ARGUMENTS, "").trim(); //$NON-NLS-1$
 		if (!argsString.isEmpty()) {
-			JsonArray args = new JsonArray();
-			Arrays.asList(argsString.split(" ")).stream() //$NON-NLS-1$
-				.filter(s -> !s.trim().isEmpty())
-				.forEach(args::add);
-			if (args.size() > 0) {
-				param.add(ARGUMENTS, args);
+			Object[] args = Arrays.asList(argsString.split(" ")).stream() //$NON-NLS-1$
+					.filter(s -> !s.trim().isEmpty()).toArray();
+			if (args.length > 0) {
+				param.put(ARGUMENTS, args);
 			}
 		}
-		Map<String, String> env = wc.getAttribute(ILaunchManager.ATTR_ENVIRONMENT_VARIABLES, Collections.emptyMap());
+		Map<String, String> env = configuration.getAttribute(ILaunchManager.ATTR_ENVIRONMENT_VARIABLES,
+				Collections.emptyMap());
 		if (!env.isEmpty()) {
 			JsonObject envJson = new JsonObject();
 			env.forEach((key, value) -> envJson.addProperty(key, value));
-			param.add(ENV, envJson);
+			param.put(ENV, envJson);
 		}
-		String cwd = wc.getAttribute(DebugPlugin.ATTR_WORKING_DIRECTORY, "").trim(); //$NON-NLS-1$
+		String cwd = configuration.getAttribute(DebugPlugin.ATTR_WORKING_DIRECTORY, "").trim(); //$NON-NLS-1$
 		if (!cwd.isEmpty()) {
-			param.addProperty(CWD, cwd);
+			param.put(CWD, cwd);
 		}
-		// internal settings
-		if (!ILaunchManager.DEBUG_MODE.equals(mode)) {
-			param.addProperty("noDebug", true); //$NON-NLS-1$
-		}
-		wc.setAttribute(DSPPlugin.ATTR_DSP_PARAM, new Gson().toJson(param));
-		wc.setAttribute(DSPPlugin.ATTR_DSP_MODE, DSPPlugin.DSP_MODE_LAUNCH);
-		wc.setAttribute(DSPPlugin.ATTR_DSP_MONITOR_DEBUG_ADAPTER, true);
-		wc.setAttribute(DSPPlugin.ATTR_DSP_CMD, InitializeLaunchConfigurations.getNodeJsLocation());
+
 		try {
-			wc.setAttribute(DSPPlugin.ATTR_DSP_ARGS, Collections.singletonList(FileLocator.toFileURL(getClass().getResource("/language-servers/node_modules/node-debug2/out/src/nodeDebug.js")).getPath())); //$NON-NLS-1$
-			configuration = wc.doSave();
-			super.launch(configuration, mode, launch, monitor);
+			URL fileURL = FileLocator.toFileURL(
+					getClass().getResource("/language-servers/node_modules/node-debug2/out/src/nodeDebug.js"));
+			List<String> debugCmdArgs = Collections.singletonList(fileURL.getPath());
+
+			DSPLaunchDelegateLaunchBuilder builder = new DSPLaunchDelegateLaunchBuilder(configuration, mode, launch,
+					monitor);
+			builder.setLaunchDebugAdapter(InitializeLaunchConfigurations.getNodeJsLocation(), debugCmdArgs);
+			builder.setMonitorDebugAdapter(true);
+			builder.setDspParameters(param);
+
+			super.launch(builder);
 		} catch (IOException e) {
 			IStatus errorStatus = new Status(IStatus.ERROR, Activator.PLUGIN_ID, e.getMessage(), e);
 			Activator.getDefault().getLog().log(errorStatus);
 			ErrorDialog.openError(Display.getDefault().getActiveShell(), "Debug error", e.getMessage(), errorStatus); //$NON-NLS-1$
 		}
-
 	}
 
 }
