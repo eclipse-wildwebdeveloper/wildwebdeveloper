c1c70e6b522603ee4335ec55a0b579d97f4d8af6
Xi Yan <xixiyan@redhat.com>
2019-01-18 11:42:03 -0500

    update build folder

--- org.eclipse.wildwebdeveloper/src/org/eclipse/wildwebdeveloper/xml/XMLLanguageServer.java
+++ org.eclipse.wildwebdeveloper/src/org/eclipse/wildwebdeveloper/xml/XMLLanguageServer.java
@@ -12,13 +12,19 @@
  *******************************************************************************/
 package org.eclipse.wildwebdeveloper.xml;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.lsp4e.server.ProcessStreamConnectionProvider;
 import org.eclipse.wildwebdeveloper.Activator;
@@ -27,10 +33,11 @@ public class XMLLanguageServer extends ProcessStreamConnectionProvider {
 
 	public XMLLanguageServer() {
 		List<String> commands = new ArrayList<>();
-		commands.add("java");
+		commands.add(computeJavaPath());
 		commands.add("-classpath");
 		try {
-			URL url = FileLocator.toFileURL(getClass().getResource("/language-servers/org.eclipse.lsp4xml-all.jar"));
+			URL url = FileLocator
+					.toFileURL(getClass().getResource("/language-servers/server/org.eclipse.lsp4xml-all.jar"));
 			commands.add(new java.io.File(url.getPath()).getAbsolutePath());
 			commands.add("org.eclipse.lsp4xml.XMLServerLauncher");
 			setCommands(commands);
@@ -39,6 +46,18 @@ public class XMLLanguageServer extends ProcessStreamConnectionProvider {
 			Activator.getDefault().getLog().log(
 					new Status(IStatus.ERROR, Activator.getDefault().getBundle().getSymbolicName(), e.getMessage(), e));
 		}
+	}
+
+	private String computeJavaPath() {
+		String javaPath = "java";
+		boolean existsInPath = Stream.of(System.getenv("PATH").split(Pattern.quote(File.pathSeparator))).map(Paths::get)
+				.anyMatch(path -> Files.exists(path.resolve("java")));
+		if (!existsInPath) {
+			File f = new File(System.getProperty("java.home"),
+					"bin/java" + (Platform.getOS().equals(Platform.OS_WIN32) ? ".exe" : ""));
+			javaPath = f.getAbsolutePath();
+		}
+		return javaPath;
 	}
 
 	@Override
