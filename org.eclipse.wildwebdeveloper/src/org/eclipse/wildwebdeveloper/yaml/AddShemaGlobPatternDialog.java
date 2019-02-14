09ba62ca7cded360845732ac81f4b30845855012
Xi Yan <xixiyan@redhat.com>
2019-02-14 12:48:03 -0500

    integrate YAML LS
    Signed-off-by: Xi Yan <xixiyan@redhat.com>
    Signed-off-by: Xi Yan <xixiyan@redhat.com>

---
+++ org.eclipse.wildwebdeveloper/src/org/eclipse/wildwebdeveloper/yaml/AddShemaGlobPatternDialog.java
@@ -0,0 +1,92 @@
+package org.eclipse.wildwebdeveloper.yaml;
+
+import org.eclipse.jface.dialogs.TitleAreaDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+
+public class AddShemaGlobPatternDialog extends TitleAreaDialog {
+	private Text schemaKeyText;
+	private Text globPatternText;
+	
+	private String schemaKey;
+	private String globPattern;
+	
+	public AddShemaGlobPatternDialog(Shell parentShell) {
+		super(parentShell);
+	}
+	
+	@Override
+	public void create() {
+		super.create();
+		setTitle("Add Schema");
+		setMessage("Associate YAML Schema");
+	}
+	
+	@Override
+	protected Control createDialogArea(Composite parent) {
+		Composite area = (Composite) super.createDialogArea(parent);
+        Composite container = new Composite(area, SWT.NONE);
+        container.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+        GridLayout layout = new GridLayout(2, false);
+        container.setLayout(layout);
+
+        createSchema(container);
+        createGlobPattern(container);
+		return area;
+	}
+	
+	private void createSchema(Composite container) {
+        Label lbtFirstName = new Label(container, SWT.NONE);
+        lbtFirstName.setText("Schema");
+
+        GridData dataFirstName = new GridData();
+        dataFirstName.grabExcessHorizontalSpace = true;
+        dataFirstName.horizontalAlignment = GridData.FILL;
+
+        schemaKeyText = new Text(container, SWT.BORDER);
+        schemaKeyText.setLayoutData(dataFirstName);
+    }
+
+    private void createGlobPattern(Composite container) {
+        Label lbtLastName = new Label(container, SWT.NONE);
+        lbtLastName.setText("Glob Pattern");
+
+        GridData dataLastName = new GridData();
+        dataLastName.grabExcessHorizontalSpace = true;
+        dataLastName.horizontalAlignment = GridData.FILL;
+        globPatternText = new Text(container, SWT.BORDER);
+        globPatternText.setLayoutData(dataLastName);
+    }
+
+    @Override
+    protected boolean isResizable() {
+        return true;
+    }
+    
+    private void saveInput() {
+    	schemaKey = schemaKeyText.getText();
+    	globPattern = globPatternText.getText();
+
+    }
+
+    @Override
+    protected void okPressed() {
+        saveInput();
+        super.okPressed();
+    }
+
+    public String getSchemaKey() {
+        return schemaKey;
+    }
+
+    public String getGlobPattern() {
+        return globPattern;
+    }
+
+}
---
+++ org.eclipse.wildwebdeveloper/src/org/eclipse/wildwebdeveloper/yaml/YAMLLanguageServer.java
@@ -0,0 +1,70 @@
+package org.eclipse.wildwebdeveloper.yaml;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.lsp4e.server.ProcessStreamConnectionProvider;
+import org.eclipse.lsp4j.DidChangeConfigurationParams;
+import org.eclipse.lsp4j.jsonrpc.messages.Message;
+import org.eclipse.lsp4j.services.LanguageServer;
+import org.eclipse.wildwebdeveloper.Activator;
+import org.eclipse.wildwebdeveloper.InitializeLaunchConfigurations;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+public class YAMLLanguageServer extends ProcessStreamConnectionProvider {
+	
+	private String cachedSchema;
+	
+	public YAMLLanguageServer() {
+		List<String> commands = new ArrayList<>();
+		commands.add(InitializeLaunchConfigurations.getNodeJsLocation());
+		try {
+			URL url = FileLocator.toFileURL(getClass()
+					.getResource("/language-servers/node_modules/yaml-language-server/out/server/src/server.js"));
+			commands.add(new java.io.File(url.getPath()).getAbsolutePath());
+			commands.add("--stdio");
+			setCommands(commands);
+			setWorkingDirectory(System.getProperty("user.dir"));
+		} catch (IOException e) {
+			Activator.getDefault().getLog().log(
+					new Status(IStatus.ERROR, Activator.getDefault().getBundle().getSymbolicName(), e.getMessage(), e));
+		}
+	}
+	
+	@Override
+	public void handleMessage(Message message, LanguageServer languageServer, URI rootUri) {
+		IPreferenceStore preferenceStore = Activator.getDefault().getPreferenceStore();
+		String schemaStr = preferenceStore.getString(YAMLPreferenceInitializer.YAML_SCHEMA_PREFERENCE);
+		if (cachedSchema == null || !schemaStr.equals(cachedSchema)) {
+			cachedSchema = schemaStr;
+			Map<String, Object> schemas = new Gson().fromJson(schemaStr, new TypeToken<HashMap<String, Object>>() {}.getType());
+			Map<String, Object> yaml = new HashMap<>();
+			yaml.put("schemas", schemas);
+			yaml.put("validate", true);
+			yaml.put("completion", true);
+			yaml.put("hover", true);
+			
+			Map<String, Object> settings = new HashMap<>();
+			settings.put("yaml", yaml);
+			
+			DidChangeConfigurationParams params = new DidChangeConfigurationParams(settings);
+			languageServer.getWorkspaceService().didChangeConfiguration(params);
+		}
+	}
+	
+	@Override
+	public String toString() {
+		return "YAML Language Server: " + super.toString();
+	}
+}
