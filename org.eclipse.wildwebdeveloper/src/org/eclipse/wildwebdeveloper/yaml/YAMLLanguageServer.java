7ab0e0bce156ffa2c44622634c4cc43afb10f55b
Mickael Istria <mistria@redhat.com>
2019-03-11 20:33:29 +0100

    Fix copyright headers
    Signed-off-by: Mickael Istria <mistria@redhat.com>

--- org.eclipse.wildwebdeveloper/src/org/eclipse/wildwebdeveloper/yaml/YAMLLanguageServer.java
+++ org.eclipse.wildwebdeveloper/src/org/eclipse/wildwebdeveloper/yaml/YAMLLanguageServer.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Red Hat Inc. and others.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
 package org.eclipse.wildwebdeveloper.yaml;
 
 import java.io.IOException;
