40cc888a8db77477bf8bff62f57b5fe10f032a9f
Mickael Istria <mistria@redhat.com>
2020-02-24 18:04:10 +0100

    Missing copyright headers
    Signed-off-by: Mickael Istria <mistria@redhat.com>

--- org.eclipse.wildwebdeveloper/src/org/eclipse/wildwebdeveloper/debug/AbstractHTMLDebugDelegate.java
+++ org.eclipse.wildwebdeveloper/src/org/eclipse/wildwebdeveloper/debug/AbstractHTMLDebugDelegate.java
@@ -6,9 +6,6 @@
  * which is available at https://www.eclipse.org/legal/epl-2.0/
  *
  * SPDX-License-Identifier: EPL-2.0
- *
- * Contributors:
- *   Andrew Obuchowicz (Red Hat Inc.) 
  *******************************************************************************/
 package org.eclipse.wildwebdeveloper.debug;
 
