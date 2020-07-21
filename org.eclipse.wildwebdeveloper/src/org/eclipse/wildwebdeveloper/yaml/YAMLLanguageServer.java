036ffb37ebf6a65d44f270db13b2425cba838c21
Victor Rubezhny <vrubezhny@redhat.com>
2020-07-21 05:11:08 +0200

    Cannot use provided schemas #461
    Fixes: #461
    
    Signed-off-by: Victor Rubezhny <vrubezhny@redhat.com>

--- org.eclipse.wildwebdeveloper/src/org/eclipse/wildwebdeveloper/yaml/YAMLLanguageServer.java
+++ org.eclipse.wildwebdeveloper/src/org/eclipse/wildwebdeveloper/yaml/YAMLLanguageServer.java
@@ -21,9 +21,16 @@ import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.util.IPropertyChangeListener;
+import org.eclipse.jface.util.PropertyChangeEvent;
+import org.eclipse.lsp4e.LanguageServersRegistry;
+import org.eclipse.lsp4e.LanguageServersRegistry.LanguageServerDefinition;
+import org.eclipse.lsp4e.LanguageServiceAccessor;
 import org.eclipse.lsp4e.server.ProcessStreamConnectionProvider;
 import org.eclipse.lsp4j.DidChangeConfigurationParams;
+import org.eclipse.lsp4j.InitializeResult;
 import org.eclipse.lsp4j.jsonrpc.messages.Message;
+import org.eclipse.lsp4j.jsonrpc.messages.ResponseMessage;
 import org.eclipse.lsp4j.services.LanguageServer;
 import org.eclipse.wildwebdeveloper.Activator;
 import org.eclipse.wildwebdeveloper.embedder.node.NodeJSManager;
@@ -31,9 +38,36 @@ import org.eclipse.wildwebdeveloper.embedder.node.NodeJSManager;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+@SuppressWarnings("restriction")
 public class YAMLLanguageServer extends ProcessStreamConnectionProvider {
+	private static final String SETTINGS_KEY = "settings";
+	private static final String YAML_KEY = "yaml";
+	private static final String VALIDATE_KEY = "validate";
+	private static final String COMPLETION_KEY = "completion";
+	private static final String HOVER_KEY = "hover";
+	private static final String SCHEMAS_KEY = "schemas";
 	
-	private String cachedSchema;
+ 	private static final IPreferenceStore store = Activator.getDefault().getPreferenceStore();
+ 	private static final LanguageServerDefinition yamlLsDefinition = LanguageServersRegistry.getInstance().getDefinition("org.eclipse.wildwebdeveloper.yaml");
+ 	private static final IPropertyChangeListener psListener = new IPropertyChangeListener() {
+		@Override
+		public void propertyChange(PropertyChangeEvent event) {
+			if (YAMLPreferenceInitializer.YAML_SCHEMA_PREFERENCE.equals(event.getProperty())) {
+				Map<String, Object> yaml = getYamlSchemaOptions();
+				Map<String, Object> settings = new HashMap<>();
+				settings.put(YAML_KEY, yaml);
+				Map<String, Object> options = new HashMap<>();
+				options.put(SETTINGS_KEY, settings);
+				DidChangeConfigurationParams params = new DidChangeConfigurationParams(settings);
+				LanguageServiceAccessor.getActiveLanguageServers(null).stream().filter(server -> yamlLsDefinition.equals(LanguageServiceAccessor.resolveServerDefinition(server).get()))
+					.forEach(ls -> ls.getWorkspaceService().didChangeConfiguration(params));
+			}
+		}
+ 	};
+ 	
+	static {
+		store.addPropertyChangeListener(psListener);
+	}
 	
 	public YAMLLanguageServer() {
 		List<String> commands = new ArrayList<>();
@@ -53,27 +87,38 @@ public class YAMLLanguageServer extends ProcessStreamConnectionProvider {
 	
 	@Override
 	public void handleMessage(Message message, LanguageServer languageServer, URI rootUri) {
-		IPreferenceStore preferenceStore = Activator.getDefault().getPreferenceStore();
-		String schemaStr = preferenceStore.getString(YAMLPreferenceInitializer.YAML_SCHEMA_PREFERENCE);
-		if (cachedSchema == null || !schemaStr.equals(cachedSchema)) {
-			cachedSchema = schemaStr;
-			Map<String, Object> schemas = new Gson().fromJson(schemaStr, new TypeToken<HashMap<String, Object>>() {}.getType());
-			Map<String, Object> yaml = new HashMap<>();
-			yaml.put("schemas", schemas);
-			yaml.put("validate", true);
-			yaml.put("completion", true);
-			yaml.put("hover", true);
-			
-			Map<String, Object> settings = new HashMap<>();
-			settings.put("yaml", yaml);
-			
-			DidChangeConfigurationParams params = new DidChangeConfigurationParams(settings);
-			languageServer.getWorkspaceService().didChangeConfiguration(params);
+		if (message instanceof ResponseMessage) {
+			ResponseMessage responseMessage = (ResponseMessage) message;
+			if (responseMessage.getResult() instanceof InitializeResult) {
+				Map<String, Object> settings = new HashMap<>();
+				settings.put(YAML_KEY, getYamlSchemaOptions());
+				
+				DidChangeConfigurationParams params = new DidChangeConfigurationParams(settings);
+				languageServer.getWorkspaceService().didChangeConfiguration(params);
+			}
 		}
 	}
-	
+
+	private static Map<String, Object> getYamlSchemaOptions() {
+		Map<String, Object> yaml = new HashMap<>();
+		IPreferenceStore preferenceStore = Activator.getDefault().getPreferenceStore();
+		String schemaStr = preferenceStore.getString(YAMLPreferenceInitializer.YAML_SCHEMA_PREFERENCE);
+		Map<String, Object> schemas = new Gson().fromJson(schemaStr, new TypeToken<HashMap<String, Object>>() {}.getType());
+		yaml.put(SCHEMAS_KEY, schemas);
+		yaml.put(VALIDATE_KEY,true);
+		yaml.put(COMPLETION_KEY, true);
+		yaml.put(HOVER_KEY, true);
+		return yaml;
+	}
+
 	@Override
 	public String toString() {
 		return "YAML Language Server: " + super.toString();
+	}
+
+	@Override
+	public void stop() {
+		store.removePropertyChangeListener(psListener);
+		super.stop();
 	}
 }
