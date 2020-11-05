ffb4acfa17272c28836173f3804bfef65b98394b
Kris De Volder <kdevolder@pivotal.io>
2020-11-05 14:28:56 -0800

    Remove CommandExecutor and use the one from lsp4e
    Signed-off-by: Kris De Volder <kdevolder@pivotal.io>

--- org.eclipse.wildwebdeveloper.xml/src/org/eclipse/wildwebdeveloper/xml/internal/XmlLanguageClientImpl.java
+++ org.eclipse.wildwebdeveloper.xml/src/org/eclipse/wildwebdeveloper/xml/internal/XmlLanguageClientImpl.java
@@ -1,21 +1,20 @@
 package org.eclipse.wildwebdeveloper.xml.internal;
 
-import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 import org.eclipse.lsp4e.LanguageClientImpl;
+import org.eclipse.lsp4e.command.CommandExecutor;
+import org.eclipse.lsp4j.Command;
 import org.eclipse.lsp4j.ExecuteCommandParams;
 
 @SuppressWarnings("restriction")
 public class XmlLanguageClientImpl extends LanguageClientImpl implements XMLLanguageClient{
-	
-	private CommandExecutor commandExecutor = new CommandExecutor();
-	
+		
 	public CompletableFuture<Object> executeClientCommand(ExecuteCommandParams params) {
-		String id = params.getCommand();
-		List<Object> args = params.getArguments();
-		return commandExecutor.executeClientCommand(id, args.toArray());
+		Command cmd = new Command();
+		cmd.setCommand(params.getCommand());
+		cmd.setArguments(params.getArguments());
+		return CommandExecutor.executeCommand(cmd, null, null);
 	}
-
 
 }
