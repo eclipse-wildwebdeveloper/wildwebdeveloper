7969c627b0c91b974d74b5ce8ca7228dcd45cdd7
Alexander Kurtakov <akurtako@redhat.com>
2021-02-19 10:04:14 +0200

    Remove useless imports.
    And enable save actions to they are autoremoved from now on.
    
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- org.eclipse.wildwebdeveloper.xml/src/org/eclipse/wildwebdeveloper/xml/internal/XMLLanguageServer.java
+++ org.eclipse.wildwebdeveloper.xml/src/org/eclipse/wildwebdeveloper/xml/internal/XMLLanguageServer.java
@@ -18,15 +18,12 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Set;
-import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 import org.eclipse.core.net.proxy.IProxyData;
