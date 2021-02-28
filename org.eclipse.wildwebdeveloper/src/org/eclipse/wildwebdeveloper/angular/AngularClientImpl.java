2498ed6680a10439e2f97b93b1116daf0db3df61
Johan Compagner <jcompagner@gmail.com>
2021-02-28 20:15:22 +0100

    implemented a LanguageClientImpl for angular
    added json request/notifications for:
    projectLoadingStart, projectLoadingFinish, projectLanguageService,
    suggestIvyLanguageServiceMode, suggestStrictMode and implemented
    notifyProgress
    
    For now these all just log, but suggestIvyLanguageServiceMode should
    give the user an option (or point to an option) to enable the
    commands.add("--experimental-ivy");
    in the AngularLanguageServer
    
    (this should i think be a setting in the preferences)
    Problem is a bit if you enabled this now that you get many exception in
    the log because of: https://github.com/eclipse/lsp4j/issues/479
    
    Signed-off-by: Johan Compagner <jcompagner@gmail.com>

---
+++ org.eclipse.wildwebdeveloper/src/org/eclipse/wildwebdeveloper/angular/AngularClientImpl.java
@@ -0,0 +1,53 @@
+package org.eclipse.wildwebdeveloper.angular;
+
+import java.util.Map;
+
+import org.eclipse.lsp4e.LanguageClientImpl;
+import org.eclipse.lsp4j.MessageParams;
+import org.eclipse.lsp4j.MessageType;
+import org.eclipse.lsp4j.ProgressParams;
+import org.eclipse.lsp4j.WorkDoneProgressNotification;
+import org.eclipse.lsp4j.WorkDoneProgressReport;
+
+public class AngularClientImpl extends LanguageClientImpl implements AngularLanguageServerExtention {
+
+	@Override
+	public void projectLoadingFinish(Object object) {
+		// TODO should this set some state because only now stuff will work like hover..
+		// or maybe even after projectLanguageService "enabled" call
+		logMessage(new MessageParams(MessageType.Info, "Angular project loading finished"));
+	}
+	
+	@Override
+	public void projectLoadingStart(Object object) {
+		logMessage(new MessageParams(MessageType.Info, "Angular project loading started"));
+	}
+	
+	@Override
+	public void projectLanguageService(Map<String,Object> data) {
+		logMessage(new MessageParams(MessageType.Info, "Language Service is " + (((Boolean)data.get("languageServiceEnabled")).booleanValue()?"":"not yet ") + "enabled for project " + data.get("projectName")));
+	}
+
+	@Override
+	public void suggestIvyLanguageServiceMode(Object o) {
+		logMessage(new MessageParams(MessageType.Info, o.toString()));
+		// TODO should this propose a setting (in the preferences) that enables --experimental-ivy command in the AngularLanguageServer)
+	}
+
+	@Override
+	public void suggestStrictMode(Object o) {
+		// this only says to the developer that they should enabled something in there tsconfig file (strictTemplates: true)
+		// can't do any from there (like  suggestIvyLanguageServiceMode has to do)
+		logMessage(new MessageParams(MessageType.Info, o.toString()));
+	}
+	
+	@Override
+	public void notifyProgress(ProgressParams params) {
+		String message = null;
+		WorkDoneProgressNotification value = params.getValue();
+		if (value instanceof WorkDoneProgressReport) {
+			message = ((WorkDoneProgressReport) value).getMessage();
+		}
+		logMessage(new MessageParams(MessageType.Info, params.getToken().getLeft()  + ": " + (message == null? "done":message)));
+	}
+}
---
+++ org.eclipse.wildwebdeveloper/src/org/eclipse/wildwebdeveloper/angular/AngularLanguageServerExtention.java
@@ -0,0 +1,23 @@
+package org.eclipse.wildwebdeveloper.angular;
+
+import java.util.Map;
+
+import org.eclipse.lsp4j.jsonrpc.services.JsonNotification;
+
+public interface AngularLanguageServerExtention {
+
+	@JsonNotification(value = "angular/projectLoadingStart")
+	public void projectLoadingStart(Object object);
+	
+	@JsonNotification(value = "angular/projectLoadingFinish")
+	public void projectLoadingFinish(Object object);
+	
+	@JsonNotification(value = "angular/projectLanguageService")
+	public void projectLanguageService(Map<String,Object> data);
+
+	@JsonNotification(value = "angular/suggestStrictMode")
+	public void suggestStrictMode(Object o);
+
+	@JsonNotification(value = "angular/suggestIvyLanguageServiceMode")
+	public void suggestIvyLanguageServiceMode(Object o);
+}
