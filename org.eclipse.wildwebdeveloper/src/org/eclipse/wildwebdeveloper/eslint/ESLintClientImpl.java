7405801556f5b780f8f3a0448be5e697b90a038a
Johan Compagner <jcompagner@gmail.com>
2021-11-22 11:59:31 +0100

    Update VSCode ESLint plugin to 2.2.2 (from 2.1.14) (server 2.1.14->2.1.25)
    support ESlint 8.0

--- org.eclipse.wildwebdeveloper/src/org/eclipse/wildwebdeveloper/eslint/ESLintClientImpl.java
+++ org.eclipse.wildwebdeveloper/src/org/eclipse/wildwebdeveloper/eslint/ESLintClientImpl.java
@@ -73,6 +73,7 @@ public class ESLintClientImpl extends LanguageClientImpl implements ESLintLangua
 		
 		config.put("validate", "on");
 		config.put("run", "onType");
+		config.put("rulesCustomizations", Collections.EMPTY_LIST);
 
 		config.put("codeAction", Map.of("disableRuleComment", Map.of("enable", "true", "location", "separateLine"), 
 										"showDocumentation", Collections.singletonMap("enable", "true")));
