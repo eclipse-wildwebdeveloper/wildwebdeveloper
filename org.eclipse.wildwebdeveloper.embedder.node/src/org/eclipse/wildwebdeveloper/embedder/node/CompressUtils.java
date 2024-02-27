7163deabd5aeafadb607cae496ab34469a6b05f6
Александър Куртаков <akurtakov@gmail.com>
2024-02-27 19:20:02 +0200

    Update commons-compress to 1.26.0
    Commons-compress deprecated its own IOUtils in favor of commons-io thus
    code is migrated accordingly.

--- org.eclipse.wildwebdeveloper.embedder.node/src/org/eclipse/wildwebdeveloper/embedder/node/CompressUtils.java
+++ org.eclipse.wildwebdeveloper.embedder.node/src/org/eclipse/wildwebdeveloper/embedder/node/CompressUtils.java
@@ -28,75 +28,75 @@ import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
 import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream;
 import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
 import org.apache.commons.compress.compressors.xz.XZCompressorInputStream;
-import org.apache.commons.compress.utils.IOUtils;
+import org.apache.commons.io.IOUtils;
 
 public class CompressUtils {
-	public static void unarchive(URL archiveURL, File baseDir) throws IOException {
-		if (archiveURL == null || baseDir == null) {
-			return;
-		}
-		ArchiveInputStream archive = null;
-		try (InputStream input = archiveURL.openStream()) {
-			if (archiveURL.getFile().endsWith(".tar.gz")) { //$NON-NLS-1$
-				InputStream gz = new GzipCompressorInputStream(input);
-				archive = new TarArchiveInputStream(gz);
-			} else if (archiveURL.getFile().endsWith(".tar.xz")) { //$NON-NLS-1$
-				InputStream xz = new XZCompressorInputStream(input);
-				archive = new TarArchiveInputStream(xz);
-			} else if (archiveURL.getFile().endsWith(".zip")) { //$NON-NLS-1$
-				archive = new ZipArchiveInputStream(input);
-			} else {
-				throw new UnsupportedCompressionAlgorithmException("Unsupported archive file extension: " + archive); //$NON-NLS-1$
-			}
-			try {
-				extractArchive(archive, baseDir);
-			} finally {
-				IOUtils.closeQuietly(archive);
-			}
-		}
-	}
-	
-	/**
-	 * Extract zip/tar.gz/tar.xz file to destination folder.
-	 * Sets up 'executable' permission for TarAchiveEntry representing an 
-	 * executable file.
-	 *
-	 * @param in
-	 *			Zip/Tar Archive Input Stream to extract
-	 * @param destination
-	 *			destination folder
-	 */
-	private static void extractArchive(ArchiveInputStream in, File destination) throws IOException {
-		ArchiveEntry entry = null;
-		while ((entry = in.getNextEntry()) != null) {
-			if (!in.canReadEntryData(entry)) {
-				// log something?
-				continue;
-			}
-			File f = new File(destination, entry.getName());
-			f.delete();
-			boolean symlink = entry instanceof TarArchiveEntry tarEntry && tarEntry.isSymbolicLink();
-			if (entry.isDirectory()) {
-				if (!f.isDirectory() && !f.mkdirs()) {
-					throw new IOException("failed to create directory " + f);
-				}
-			} else {
-				File parent = f.getParentFile();
-				if (!parent.isDirectory() && !parent.mkdirs()) {
-					throw new IOException("failed to create directory " + parent);
-				}
-				if (symlink) {
-					String linkName = ((TarArchiveEntry)entry).getLinkName();
-					Files.createSymbolicLink(f.toPath(), Paths.get(linkName));					
-				} else {
-					try (OutputStream o = Files.newOutputStream(f.toPath())) {
-						IOUtils.copy(in, o);
-					}
-				}
-				if (entry instanceof TarArchiveEntry tarEntry) {
-					f.setExecutable((tarEntry.getMode() & 256) != 0);
-				}
-			}
-		}
-	}
+    public static void unarchive(URL archiveURL, File baseDir) throws IOException {
+        if (archiveURL == null || baseDir == null) {
+            return;
+        }
+        ArchiveInputStream archive = null;
+        try (InputStream input = archiveURL.openStream()) {
+            if (archiveURL.getFile().endsWith(".tar.gz")) { //$NON-NLS-1$
+                InputStream gz = new GzipCompressorInputStream(input);
+                archive = new TarArchiveInputStream(gz);
+            } else if (archiveURL.getFile().endsWith(".tar.xz")) { //$NON-NLS-1$
+                InputStream xz = new XZCompressorInputStream(input);
+                archive = new TarArchiveInputStream(xz);
+            } else if (archiveURL.getFile().endsWith(".zip")) { //$NON-NLS-1$
+                archive = new ZipArchiveInputStream(input);
+            } else {
+                throw new UnsupportedCompressionAlgorithmException("Unsupported archive file extension: " + archive); //$NON-NLS-1$
+            }
+            try {
+                extractArchive(archive, baseDir);
+            } finally {
+                IOUtils.closeQuietly(archive);
+            }
+        }
+    }
+
+    /**
+     * Extract zip/tar.gz/tar.xz file to destination folder.
+     * Sets up 'executable' permission for TarAchiveEntry representing an
+     * executable file.
+     *
+     * @param in
+     *                    Zip/Tar Archive Input Stream to extract
+     * @param destination
+     *                    destination folder
+     */
+    private static void extractArchive(ArchiveInputStream in, File destination) throws IOException {
+        ArchiveEntry entry = null;
+        while ((entry = in.getNextEntry()) != null) {
+            if (!in.canReadEntryData(entry)) {
+                // log something?
+                continue;
+            }
+            File f = new File(destination, entry.getName());
+            f.delete();
+            boolean symlink = entry instanceof TarArchiveEntry tarEntry && tarEntry.isSymbolicLink();
+            if (entry.isDirectory()) {
+                if (!f.isDirectory() && !f.mkdirs()) {
+                    throw new IOException("failed to create directory " + f);
+                }
+            } else {
+                File parent = f.getParentFile();
+                if (!parent.isDirectory() && !parent.mkdirs()) {
+                    throw new IOException("failed to create directory " + parent);
+                }
+                if (symlink) {
+                    String linkName = ((TarArchiveEntry) entry).getLinkName();
+                    Files.createSymbolicLink(f.toPath(), Paths.get(linkName));
+                } else {
+                    try (OutputStream o = Files.newOutputStream(f.toPath())) {
+                        IOUtils.copy(in, o);
+                    }
+                }
+                if (entry instanceof TarArchiveEntry tarEntry) {
+                    f.setExecutable((tarEntry.getMode() & 256) != 0);
+                }
+            }
+        }
+    }
 }
