66fb1ee08444caff66ce1831de897bbe2ba290a3
Александър Куртаков <akurtakov@gmail.com>
2024-11-18 16:56:40 +0200

    Fix warnings in tests about not closed autoclosables

--- org.eclipse.wildwebdeveloper.tests/src/org/eclipse/wildwebdeveloper/tests/TestESLint.java
+++ org.eclipse.wildwebdeveloper.tests/src/org/eclipse/wildwebdeveloper/tests/TestESLint.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright (c) 2019, 2023 Red Hat Inc. and others.
+* Copyright (c) 2019, 2024 Red Hat Inc. and others.
 *
 * This program and the accompanying materials are made
 * available under the terms of the Eclipse Public License 2.0
@@ -15,10 +15,8 @@ package org.eclipse.wildwebdeveloper.tests;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.util.Arrays;
 import java.util.Objects;
@@ -44,122 +42,120 @@ import org.junit.jupiter.api.Test;
 
 class TestESLint {
 
-	private static IProject project;
+    private static IProject project;
 
-	@BeforeAll
-	public static void setUpProject() throws Exception {
-		AllCleanRule.closeIntro();
-		AllCleanRule.enableLogging();
+    @BeforeAll
+    public static void setUpProject() throws Exception {
+        AllCleanRule.closeIntro();
+        AllCleanRule.enableLogging();
 
-		String projectName = TestESLint.class.getName() + System.nanoTime();
-		IProjectDescription desc = ResourcesPlugin.getWorkspace().newProjectDescription(projectName);
-		IPath projectLocation = ResourcesPlugin.getWorkspace().getRoot().getLocation().append(projectName);
-		desc.setLocation(projectLocation);
-		File projectDirectory = projectLocation.toFile();
-		projectDirectory.mkdir();
-		try (InputStream eslintRc = TestESLint.class.getResourceAsStream("/testProjects/eslint/.eslintrc")) {
-			Files.copy(eslintRc, new File(projectDirectory, ".eslintrc").toPath());
-		}
-		try (InputStream eslintRc = TestESLint.class.getResourceAsStream("/testProjects/eslint/tsconfig.json")) {
-			Files.copy(eslintRc, new File(projectDirectory, "tsconfig.json").toPath());
-		}
-		try (InputStream eslintRc = TestESLint.class.getResourceAsStream("/testProjects/eslint/package.json")) {
-			Files.copy(eslintRc, new File(projectDirectory, "package.json").toPath());
-		}
-		try (InputStream eslintRc = TestESLint.class.getResourceAsStream("/testProjects/eslint/ESLintProj.js")) {
-			Files.copy(eslintRc, new File(projectDirectory, "ESLintProj.js").toPath());
-		}
-		try (InputStream eslintRc = TestESLint.class.getResourceAsStream("/testProjects/eslint/ESLintProj.js")) {
-			Files.copy(eslintRc, new File(projectDirectory, "ESLintProj.jsx").toPath());
-		}
-		try (InputStream eslintRc = TestESLint.class.getResourceAsStream("/testProjects/eslint/ESLintProj.js")) {
-			Files.copy(eslintRc, new File(projectDirectory, "ESLintProj.ts").toPath());
-		}
-		try (InputStream eslintRc = TestESLint.class.getResourceAsStream("/testProjects/eslint/ESLintProj.js")) {
-			Files.copy(eslintRc, new File(projectDirectory, "ESLintProj.tsx").toPath());
-		}
-		ProcessBuilder builder = NodeJSManager.prepareNPMProcessBuilder("install", "--no-bin-links", "--ignore-scripts")
-				.directory(projectDirectory);
-		Process dependencyInstaller = builder.start();
-		System.out.println(builder.command().toString());
-		String result = new BufferedReader(new InputStreamReader(dependencyInstaller.getErrorStream())).lines()
-				.collect(Collectors.joining("\n"));
-		System.out.println("Error Stream: >>>\n" + result + "\n<<<");
+        String projectName = TestESLint.class.getName() + System.nanoTime();
+        IProjectDescription desc = ResourcesPlugin.getWorkspace().newProjectDescription(projectName);
+        IPath projectLocation = ResourcesPlugin.getWorkspace().getRoot().getLocation().append(projectName);
+        desc.setLocation(projectLocation);
+        File projectDirectory = projectLocation.toFile();
+        projectDirectory.mkdir();
+        try (InputStream eslintRc = TestESLint.class.getResourceAsStream("/testProjects/eslint/.eslintrc")) {
+            Files.copy(eslintRc, new File(projectDirectory, ".eslintrc").toPath());
+        }
+        try (InputStream eslintRc = TestESLint.class.getResourceAsStream("/testProjects/eslint/tsconfig.json")) {
+            Files.copy(eslintRc, new File(projectDirectory, "tsconfig.json").toPath());
+        }
+        try (InputStream eslintRc = TestESLint.class.getResourceAsStream("/testProjects/eslint/package.json")) {
+            Files.copy(eslintRc, new File(projectDirectory, "package.json").toPath());
+        }
+        try (InputStream eslintRc = TestESLint.class.getResourceAsStream("/testProjects/eslint/ESLintProj.js")) {
+            Files.copy(eslintRc, new File(projectDirectory, "ESLintProj.js").toPath());
+        }
+        try (InputStream eslintRc = TestESLint.class.getResourceAsStream("/testProjects/eslint/ESLintProj.js")) {
+            Files.copy(eslintRc, new File(projectDirectory, "ESLintProj.jsx").toPath());
+        }
+        try (InputStream eslintRc = TestESLint.class.getResourceAsStream("/testProjects/eslint/ESLintProj.js")) {
+            Files.copy(eslintRc, new File(projectDirectory, "ESLintProj.ts").toPath());
+        }
+        try (InputStream eslintRc = TestESLint.class.getResourceAsStream("/testProjects/eslint/ESLintProj.js")) {
+            Files.copy(eslintRc, new File(projectDirectory, "ESLintProj.tsx").toPath());
+        }
+        ProcessBuilder builder = NodeJSManager.prepareNPMProcessBuilder("install", "--no-bin-links", "--ignore-scripts")
+                .directory(projectDirectory);
+        Process dependencyInstaller = builder.start();
+        System.out.println(builder.command().toString());
+        String result = dependencyInstaller.errorReader().lines().collect(Collectors.joining("\n"));
+        System.out.println("Error Stream: >>>\n" + result + "\n<<<");
 
-		result = new BufferedReader(new InputStreamReader(dependencyInstaller.getInputStream())).lines()
-				.collect(Collectors.joining("\n"));
-		System.out.println("Output Stream: >>>\n" + result + "\n<<<");
+        result = dependencyInstaller.inputReader().lines().collect(Collectors.joining("\n"));
+        System.out.println("Output Stream: >>>\n" + result + "\n<<<");
 
-		assertEquals(0, dependencyInstaller.waitFor(), "npm install didn't complete properly");
-		project = ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
-		project.create(desc, null);
-		project.open(null);
-	}
+        assertEquals(0, dependencyInstaller.waitFor(), "npm install didn't complete properly");
+        project = ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
+        project.create(desc, null);
+        project.open(null);
+    }
 
-	@BeforeEach
-	public void setUpTestCase() {
-		AllCleanRule.enableLogging();
-	}
+    @BeforeEach
+    public void setUpTestCase() {
+        AllCleanRule.enableLogging();
+    }
 
-	@AfterAll
-	public static void cleanUp() throws Exception {
-		new AllCleanRule().afterEach(null);
-	}
+    @AfterAll
+    public static void cleanUp() throws Exception {
+        new AllCleanRule().afterEach(null);
+    }
 
-	@Test
-	void testESLintDiagnosticsInJS() throws Exception {
-		IFile file = project.getFile("ESLintProj.js");
-		IDE.openEditor(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage(), file);
-		assertESLintIndentMarkerExists(file);
-	}
+    @Test
+    void testESLintDiagnosticsInJS() throws Exception {
+        IFile file = project.getFile("ESLintProj.js");
+        IDE.openEditor(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage(), file);
+        assertESLintIndentMarkerExists(file);
+    }
 
-	@Test
-	void testESLintDiagnosticsInTS() throws Exception {
-		IFile file = project.getFile("ESLintProj.ts");
-		IDE.openEditor(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage(), file);
-		assertESLintIndentMarkerExists(file);
-	}
+    @Test
+    void testESLintDiagnosticsInTS() throws Exception {
+        IFile file = project.getFile("ESLintProj.ts");
+        IDE.openEditor(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage(), file);
+        assertESLintIndentMarkerExists(file);
+    }
 
-	@Test
-	void testESLintDiagnosticsInJSX() throws Exception {
-		IFile file = project.getFile("ESLintProj.jsx");
-		IDE.openEditor(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage(), file);
-		assertESLintIndentMarkerExists(file);
-	}
+    @Test
+    void testESLintDiagnosticsInJSX() throws Exception {
+        IFile file = project.getFile("ESLintProj.jsx");
+        IDE.openEditor(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage(), file);
+        assertESLintIndentMarkerExists(file);
+    }
 
-	@Test
-	void testESLintDiagnosticsInTSX() throws Exception {
-		IFile file = project.getFile("ESLintProj.tsx");
-		IDE.openEditor(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage(), file);
-		assertESLintIndentMarkerExists(file);
-	}
+    @Test
+    void testESLintDiagnosticsInTSX() throws Exception {
+        IFile file = project.getFile("ESLintProj.tsx");
+        IDE.openEditor(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage(), file);
+        assertESLintIndentMarkerExists(file);
+    }
 
-	private void assertESLintIndentMarkerExists(IFile file) throws PartInitException {
-		IDE.openEditor(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage(), file);
-		assertTrue(new DisplayHelper() {
-			@Override
-			protected boolean condition() {
-				try {
-					return file.findMarkers("org.eclipse.lsp4e.diagnostic", true, IResource.DEPTH_ZERO).length != 0;
-				} catch (CoreException e) {
-					e.printStackTrace();
-					return false;
-				}
-			}
-		}.waitForCondition(PlatformUI.getWorkbench().getDisplay(), 10000), "Diagnostic not published");
+    private void assertESLintIndentMarkerExists(IFile file) throws PartInitException {
+        IDE.openEditor(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage(), file);
+        assertTrue(new DisplayHelper() {
+            @Override
+            protected boolean condition() {
+                try {
+                    return file.findMarkers("org.eclipse.lsp4e.diagnostic", true, IResource.DEPTH_ZERO).length != 0;
+                } catch (CoreException e) {
+                    e.printStackTrace();
+                    return false;
+                }
+            }
+        }.waitForCondition(PlatformUI.getWorkbench().getDisplay(), 10000), "Diagnostic not published");
 
-		assertTrue(new DisplayHelper() {
-			@Override
-			protected boolean condition() {
-				try {
-					return Arrays.asList(file.findMarkers("org.eclipse.lsp4e.diagnostic", true, IResource.DEPTH_ZERO))
-							.stream().filter(Objects::nonNull)
-							.anyMatch(m -> m.getAttribute(IMarker.MESSAGE, null).toLowerCase().contains("indentation"));
-				} catch (CoreException e) {
-					e.printStackTrace();
-					return false;
-				}
-			}
-		}.waitForCondition(PlatformUI.getWorkbench().getDisplay(), 5000), "Diagnostic content is incorrect");
-	}
+        assertTrue(new DisplayHelper() {
+            @Override
+            protected boolean condition() {
+                try {
+                    return Arrays.asList(file.findMarkers("org.eclipse.lsp4e.diagnostic", true, IResource.DEPTH_ZERO))
+                            .stream().filter(Objects::nonNull)
+                            .anyMatch(m -> m.getAttribute(IMarker.MESSAGE, null).toLowerCase().contains("indentation"));
+                } catch (CoreException e) {
+                    e.printStackTrace();
+                    return false;
+                }
+            }
+        }.waitForCondition(PlatformUI.getWorkbench().getDisplay(), 5000), "Diagnostic content is incorrect");
+    }
 }
--- org.eclipse.wildwebdeveloper.tests/src/org/eclipse/wildwebdeveloper/tests/TestNpmRun.java
+++ org.eclipse.wildwebdeveloper.tests/src/org/eclipse/wildwebdeveloper/tests/TestNpmRun.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2022 Red Hat Inc. and others.
+ * Copyright (c) 2022, 2024 Red Hat Inc. and others.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -14,11 +14,9 @@ package org.eclipse.wildwebdeveloper.tests;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
+import java.nio.file.Files;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
@@ -43,60 +41,60 @@ import org.junit.jupiter.api.extension.ExtendWith;
 @SuppressWarnings("restriction")
 @ExtendWith(AllCleanRule.class)
 public class TestNpmRun {
-	protected ILaunchManager launchManager;
+    protected ILaunchManager launchManager;
 
-	@BeforeEach
-	public void setUpLaunch() throws DebugException {
-		this.launchManager = DebugPlugin.getDefault().getLaunchManager();
-		removeAllLaunches();
-	}
+    @BeforeEach
+    public void setUpLaunch() throws DebugException {
+        this.launchManager = DebugPlugin.getDefault().getLaunchManager();
+        removeAllLaunches();
+    }
 
-	private void removeAllLaunches() throws DebugException {
-		for (ILaunch launch : this.launchManager.getLaunches()) {
-			launch.terminate();
-			for (IDebugTarget debugTarget : launch.getDebugTargets()) {
-				debugTarget.terminate();
-				launch.removeDebugTarget(debugTarget);
-			}
-			for (IProcess process : launch.getProcesses()) {
-				process.terminate();
-			}
-		}
-	}
+    private void removeAllLaunches() throws DebugException {
+        for (ILaunch launch : this.launchManager.getLaunches()) {
+            launch.terminate();
+            for (IDebugTarget debugTarget : launch.getDebugTargets()) {
+                debugTarget.terminate();
+                launch.removeDebugTarget(debugTarget);
+            }
+            for (IProcess process : launch.getProcesses()) {
+                process.terminate();
+            }
+        }
+    }
 
-	@AfterEach
-	public void tearDownLaunch() throws DebugException {
-		removeAllLaunches();
-	}
+    @AfterEach
+    public void tearDownLaunch() throws DebugException {
+        removeAllLaunches();
+    }
 
-	@Test
-	public void testNpmRunEnvVariables() throws Exception {
-		IProject project = Utils.provisionTestProject("envtest");
-		IFile projectFile = project.getFile("package.json");
+    @Test
+    public void testNpmRunEnvVariables() throws Exception {
+        IProject project = Utils.provisionTestProject("envtest");
+        IFile projectFile = project.getFile("package.json");
 
-		ILaunchConfigurationWorkingCopy launchConfig = launchManager.getLaunchConfigurationType(NpmLaunchDelegate.ID)
-				.newInstance(ResourcesPlugin.getWorkspace().getRoot(), "npm.envtest");
+        ILaunchConfigurationWorkingCopy launchConfig = launchManager.getLaunchConfigurationType(NpmLaunchDelegate.ID)
+                .newInstance(ResourcesPlugin.getWorkspace().getRoot(), "npm.envtest");
 
-		launchConfig.setAttribute(LaunchConstants.PROGRAM, projectFile.getLocation().toOSString());
-		launchConfig.setAttribute(DebugPlugin.ATTR_WORKING_DIRECTORY, project.getLocation().toOSString());
-		launchConfig.setAttribute(NpmLaunchDelegate.ARGUMENTS, "run envtest");
-		launchConfig.setAttribute(ILaunchManager.ATTR_ENVIRONMENT_VARIABLES,
-				Map.of("ENVTEST_ENVIRONMENT_VARIABLE", "ENVTEST_VARIABLE_VALUE}"));
-		launchConfig.setAttribute(DebugPlugin.ATTR_CAPTURE_OUTPUT, true);
-		ILaunch launch = launchConfig.launch(ILaunchManager.RUN_MODE, new NullProgressMonitor());
-		while (!launch.isTerminated()) {
-			DisplayHelper.sleep(Display.getDefault(), 50);
-		}
+        launchConfig.setAttribute(LaunchConstants.PROGRAM, projectFile.getLocation().toOSString());
+        launchConfig.setAttribute(DebugPlugin.ATTR_WORKING_DIRECTORY, project.getLocation().toOSString());
+        launchConfig.setAttribute(NpmLaunchDelegate.ARGUMENTS, "run envtest");
+        launchConfig.setAttribute(ILaunchManager.ATTR_ENVIRONMENT_VARIABLES,
+                Map.of("ENVTEST_ENVIRONMENT_VARIABLE", "ENVTEST_VARIABLE_VALUE}"));
+        launchConfig.setAttribute(DebugPlugin.ATTR_CAPTURE_OUTPUT, true);
+        ILaunch launch = launchConfig.launch(ILaunchManager.RUN_MODE, new NullProgressMonitor());
+        while (!launch.isTerminated()) {
+            DisplayHelper.sleep(Display.getDefault(), 50);
+        }
 
-		// Console message stream from a child process remains blocked until the test
-		// case finish, so we'll test the output file created instead of trying to read
-		// Console document
-		final File envtestLogFile = new File(project.getLocation().toOSString(), "envtest.dump");
-		assertTrue(envtestLogFile.exists() && envtestLogFile.canRead(),
-				"Env variable test result dump file is not created");
+        // Console message stream from a child process remains blocked until the test
+        // case finish, so we'll test the output file created instead of trying to read
+        // Console document
+        final File envtestLogFile = new File(project.getLocation().toOSString(), "envtest.dump");
+        assertTrue(envtestLogFile.exists() && envtestLogFile.canRead(),
+                "Env variable test result dump file is not created");
 
-		String content = new BufferedReader(new FileReader(envtestLogFile)).lines().collect(Collectors.joining("\n"));
-		assertTrue(content.contains("ENVTEST_ENVIRONMENT_VARIABLE") && content.contains("ENVTEST_VARIABLE_VALUE"),
-				"Env variable aren't not passed to subprocess");
-	}
+        String content = Files.readString(envtestLogFile.toPath());
+        assertTrue(content.contains("ENVTEST_ENVIRONMENT_VARIABLE") && content.contains("ENVTEST_VARIABLE_VALUE"),
+                "Env variable aren't not passed to subprocess");
+    }
 }
