c2e40decf2ed867d67763561c43f1122456da7ff
Александър Куртаков <akurtakov@gmail.com>
2026-04-18 17:54:21 +0300

    Upgrade vue-app test project lint toolchain to ESLint 10

--- org.eclipse.wildwebdeveloper.tests/testProjects/vue-app/eslint.config.mjs
+++ org.eclipse.wildwebdeveloper.tests/testProjects/vue-app/eslint.config.mjs
@@ -1,15 +1,7 @@
 import vue from 'eslint-plugin-vue';
-import babelParser from '@babel/eslint-parser';
 import js from '@eslint/js';
 
 export default [
 	js.configs.recommended,
-	...vue.configs['flat/vue3-essential'],
-	{
-		languageOptions: {
-			parserOptions: {
-				parser: babelParser,
-			},
-		},
-	},
+	...vue.configs['flat/essential'],
 ];
